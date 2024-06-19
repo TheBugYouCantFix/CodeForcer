525b8df509febab2743f7547924e6807f4b0adc7
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-06-19 18:30:11 +0300

    fix: fixed db name typo

--- src/backend/students_db_creation.py
+++ src/backend/students_db_creation.py
@@ -1,8 +1,8 @@
-import sqlite3
 from db_context import DBContext
 
+
 def create_students_db() -> None:
-    db_context = DBContext('studens.db')
+    db_context = DBContext('students.db')
     db_context.execute_command("""
         CREATE TABLE IF NOT EXISTS students (
         email TEXT PRIMARY KEY,
