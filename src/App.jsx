8e258659470a8949cf499d591ffa755508909eef
mashfeii-laptop <evil-corporaty@yandex.ru>
2024-06-21 19:52:00 +0300

    Made protected route for 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import GlobalStyles from "./styles/GlobalStyles.js";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AppLayout from "./ui/AppLayout.jsx";
 import Home from "./pages/Home.jsx";
+import Handles from "./pages/Handles.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
 import { DarkModeProvider } from "./context/DarkModeContext.jsx";
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="home" />} />
             <Route path="home" element={<Home />} />
+            <Route path="handles" element={<Handles />} />
+            <Route path="*" element={<PageNotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
