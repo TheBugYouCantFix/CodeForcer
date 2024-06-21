a8977a826e2713207090380b0acb881d728a0317
mashfeii-laptop <evil-corporaty@yandex.ru>
2024-06-21 04:12:59 +0300

    Added routes for pages, made an initial

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import GlobalStyles from "./styles/GlobalStyles.js";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./ui/AppLayout.jsx";
+import Home from "./pages/Home.jsx";
+import { DarkModeProvider } from "./context/DarkModeContext.jsx";
+
+function App() {
+  return (
+    <DarkModeProvider>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="home" />} />
+            <Route path="home" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </DarkModeProvider>
+  );
+}
+
+export default App;
