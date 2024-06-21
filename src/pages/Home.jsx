a3e68b4ee7226c8bcb5d9d79579cf7d6e78daa5a
mashfeii-laptop <evil-corporaty@yandex.ru>
2024-06-21 04:14:48 +0300

    Added the main page, blank for now

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import Row from "../ui/Row.jsx";
+import Card from "../ui/Card.jsx";
+
+function Home() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <Row>
+        <Card>
+          <h2>Hello there!</h2>
+        </Card>
+        <Card>
+          <h2>Hello there!</h2>
+        </Card>
+        <Card>
+          <h2>Hello there!</h2>
+        </Card>
+      </Row>
+    </>
+  );
+}
+
+export default Home;
