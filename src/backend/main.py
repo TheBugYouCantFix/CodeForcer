412fef37d28b047090e6b952c194a391d2d14fe7
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-06-27 01:01:20 +0300

    refactor: refined import of container into main.py

--- src/backend/main.py
+++ src/backend/main.py
@@ -4,10 +4,8 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.responses import JSONResponse
 
 from domain.student import Student
-from application.students.students_service import StudentsService
-from infrastructure.storage.db_students_repository import DBStudentsRepository
 from contracts.student_data import StudentData
-import container
+from container import students_service, contests_service
 
 app = FastAPI()
 
@@ -22,38 +20,38 @@ async def http_exception_handler(request, exc):
 
 @app.post("/students", status_code=status.HTTP_201_CREATED)
 async def create_student(student_data: StudentData) -> Student:
-    return container.students_service.create_student(student_data)
+    return students_service.create_student(student_data)
 
 
 @app.get("/students/{email_or_handle}", status_code=status.HTTP_200_OK)
 async def get_student_by_email_or_handle(email_or_handle: str) -> Student:
-    return container.students_service.get_student_by_email_or_handle(email_or_handle)
+    return students_service.get_student_by_email_or_handle(email_or_handle)
 
 
 @app.put("/students/{email}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_student(email: str, updated_student_data: StudentData) -> None:
-    container.students_service.update_student(email, updated_student_data)
+    students_service.update_student(email, updated_student_data)
 
 
 @app.delete("/students/{email}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_student(email: str) -> None:
-    container.students_service.delete_student(email)
+    students_service.delete_student(email)
 
 
 @app.get("/contests/{contest_id}/results", status_code=status.HTTP_200_OK)
 async def get_results(contest_id: int, key: str, secret: str):
-    return container.contests_service.get_contest_results(contest_id, key, secret)
+    return contests_service.get_contest_results(contest_id, key, secret)
 
 
 #not implemented yet
 @app.get("/contests/{contest_id}", status_code=status.HTTP_200_OK)
 async def get_contest(contest_id: int, key: str, secret: str):
-    return container.contests_service.get_contest(contest_id, key, secret)
+    return contests_service.get_contest(contest_id, key, secret)
 
 
 @app.post("/upload-csv", status_code=status.HTTP_201_CREATED)
 async def upload_csv(file: UploadFile = File(...)):
-    service.process_csv_file(file)
+    students_service.process_csv_file(file)
     return {"message": "CSV file processed successfully"}
 
 if __name__ == '__main__':
