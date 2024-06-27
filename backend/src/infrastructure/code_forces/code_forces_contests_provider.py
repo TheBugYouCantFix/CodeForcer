9b748ec80f16a8d023b00a41d657f06c30998803
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-06-27 12:48:32 +0300

    refactor: merged scrap_results function into get_contest_results method

--- backend/src/infrastructure/code_forces/code_forces_contests_provider.py
+++ backend/src/infrastructure/code_forces/code_forces_contests_provider.py
@@ -6,18 +6,12 @@ from results_scrapping_fields.standings_fields import *
 class CodeForcesContestsProvider(IContestsProvider):
     def get_contest_results(self, contest_id: int, key: str, secret: str):
         request_sender = CodeForcesRequestSender(key, secret)
-        return scrap_results(request_sender, contest_id)
+        result = request_sender.contest_standings(contest_id)
+
+        return [
+            {"handle": row.party.members[0].handle, "result": row.points}
+            for row in result.rows
+        ]
 
     def get_contest(self, contest_id: int, key: str, secret: str) -> Contest:
-        pass #This has to be implemented
-
-
-def scrap_results(request_sender: CodeForcesRequestSender, contest_id: int):
-    result = request_sender.contest_standings(contest_id)
-
-    extracted_results = [
-        {"handle": row.party.members[0].handle, "result": row.points}
-        for row in result.rows
-    ]
-
-    return extracted_results
+        pass # This has to be implemented
