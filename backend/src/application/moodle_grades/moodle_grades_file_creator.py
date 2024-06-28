46f939368a912c62723f57cecbf5e2a16d3ce35a
alexg <alex.gorbanev05@gmail.com>
2024-06-29 01:56:37 +0500

    refactor: added separate methods for marking grades, plagiarism and writing to file

--- backend/src/application/moodle_grades/moodle_grades_file_creator.py
+++ backend/src/application/moodle_grades/moodle_grades_file_creator.py
@@ -7,7 +7,7 @@ from contracts.moodle_results_data import MoodleResultsData, ProblemData
 
 
 class MoodleGradesFileCreator:
-    student_grade_map: Dict[str, float] = {}
+    student_grade_map: Dict[str, list[float | str]] = {}
 
     def create_file(self, results_data: MoodleResultsData) -> tuple[io.StringIO, str]:
         filename = f"moodle_grades_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
@@ -17,23 +17,30 @@ class MoodleGradesFileCreator:
             writer = csv.writer(file)
             writer.writerow(['Email', 'Grade', 'Feedback'])
 
-            for problem in results_data.contest.problems:
-                self.update_grades(problem)
+            self.mark_grades(results_data.contest.problems)
+            self.mark_plagiarism(results_data.plagiarizers)
 
-            for email, grade in self.student_grade_map.items():
-                if email in results_data.plagiarizers:
-                    self.student_grade_map[email] = 0
-                    writer.writerow([email, 0, 'Plagiarism detected'])
-                else:
-                    writer.writerow([email, grade, ''])
+            self.write_to_file(writer)
 
         return file, filename
+
+    def mark_grades(self, problems: list[ProblemData]) -> None:
+        for problem in problems:
+            self.update_grades(problem)
 
     def update_grades(self, problem: ProblemData) -> None:
         for submission in problem.submissions:
             problem_points = submission.points / problem.max_points * problem.max_grade
 
             if self.student_grade_map.get(submission.author_email) is None:
-                self.student_grade_map[submission.author_email] = problem_points
+                self.student_grade_map[submission.author_email] = [problem_points, '']
             else:
-                self.student_grade_map[submission.author_email] += problem_points
+                self.student_grade_map[submission.author_email][0] += problem_points
+
+    def mark_plagiarism(self, plagiarizers: list[str]) -> None:
+        for email in plagiarizers:
+            self.student_grade_map[email] = [0, 'Plagiarism detected']
+
+    def write_to_file(self, writer: csv.writer) -> None:
+        for email, response in self.student_grade_map.items():
+            writer.writerow([email, response[0], response[1]])
