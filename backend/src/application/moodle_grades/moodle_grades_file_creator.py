c0e9f3cb5332f4ee108496e4a5d5c58a69bec361
alexg <alex.gorbanev05@gmail.com>
2024-06-29 15:59:12 +0500

    fix: made student_grade_map a method object instead of a field

--- backend/src/application/moodle_grades/moodle_grades_file_creator.py
+++ backend/src/application/moodle_grades/moodle_grades_file_creator.py
@@ -7,37 +7,39 @@ from contracts.moodle_results_data import MoodleResultsData, ProblemData
 
 
 class MoodleGradesFileCreator:
-    student_grade_map: defaultdict[str, list[float | str]] = defaultdict(lambda: [0, ''])
-
     def create_file(self, results_data: MoodleResultsData) -> tuple[io.StringIO, str]:
         filename = f"moodle_grades_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+        student_grade_map: defaultdict[str, list[float | str]] = defaultdict(lambda: [0, ''])
 
         with open(filename, 'w'):
             file = io.StringIO()
             writer = csv.writer(file)
             writer.writerow(['Email', 'Grade', 'Feedback'])
 
-            self.mark_grades(results_data.contest.problems)
-            self.mark_plagiarism(results_data.plagiarizers)
+            self.mark_grades(results_data.contest.problems, student_grade_map)
+            self.mark_plagiarism(results_data.plagiarizers, student_grade_map)
 
-            self.write_to_file(writer)
+            self.write_to_file(writer, student_grade_map)
 
         return file, filename
 
-    def mark_grades(self, problems: list[ProblemData]) -> None:
+    def mark_grades(self, problems: list[ProblemData], student_grade_map: defaultdict[str, list[float | str]]) -> None:
         for problem in problems:
-            self.update_grades(problem)
+            self.update_grades(problem, student_grade_map)
 
-    def update_grades(self, problem: ProblemData) -> None:
+    @staticmethod
+    def update_grades(problem: ProblemData, student_grade_map: defaultdict[str, list[float | str]]) -> None:
         for submission in problem.submissions:
             problem_points = submission.points / problem.max_points * problem.max_grade
 
-            self.student_grade_map[submission.author_email][0] += problem_points
+            student_grade_map[submission.author_email][0] += problem_points
 
-    def mark_plagiarism(self, plagiarizers: list[str]) -> None:
+    @staticmethod
+    def mark_plagiarism(plagiarizers: list[str], student_grade_map: defaultdict[str, list[float | str]]) -> None:
         for email in plagiarizers:
-            self.student_grade_map[email] = [0, 'Plagiarism detected']
+            student_grade_map[email] = [0, 'Plagiarism detected']
 
-    def write_to_file(self, writer: csv.writer) -> None:
-        for email, (grade, feedback) in self.student_grade_map.items():
+    @staticmethod
+    def write_to_file(writer: csv.writer, student_grade_map: defaultdict[str, list[float | str]]) -> None:
+        for email, (grade, feedback) in student_grade_map.items():
             writer.writerow([email, grade, feedback])
