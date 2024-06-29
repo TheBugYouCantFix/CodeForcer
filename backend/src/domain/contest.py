1f0092ca5f3079273bc94781eb855352cb1dd58f
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-06-29 17:26:36 +0300

    fix: replaces ContestParticipant with Student with nullable email

--- backend/src/domain/contest.py
+++ backend/src/domain/contest.py
@@ -5,7 +5,7 @@ from typing import Callable
 from pydantic import BaseModel, EmailStr
 
 from domain.enums import Phase, Verdict
-from domain.student import ContestParticipant, Student
+from domain.student import Student
 
 
 class Contest(BaseModel):
@@ -18,12 +18,12 @@ class Contest(BaseModel):
         for problem in self.problems:
             problem.map_handles_to_emails(handle_to_email_mapper)
 
-    def select_single_submission_for_each_student(self, selector: Callable[[list[Submission]], Submission]) -> None:
+    def select_single_submission_for_each_participant(self, selector: Callable[[list[Submission]], Submission]) -> None:
         for problem in self.problems:
-            problem.select_single_submission_for_each_student(selector)
+            problem.select_single_submission_for_each_participant(selector)
 
     @property
-    def get_participants(self) -> set[ContestParticipant]:
+    def get_participants(self) -> set[Student]:
         return {
             participant
             for problem in self.problems
@@ -41,7 +41,7 @@ class Problem(BaseModel):
         for submission in self.submissions:
             submission.map_author_handle_to_email(handle_to_email_mapper)
 
-    def select_single_submission_for_each_student(self, selector: Callable[[list[Submission]], Submission]) -> None:
+    def select_single_submission_for_each_participant(self, selector: Callable[[list[Submission]], Submission]) -> None:
         submissions_by_student = defaultdict(list[Submission])
 
         for submission in self.submissions:
@@ -56,25 +56,18 @@ class Problem(BaseModel):
         self.submissions = selected_submissions
 
     @property
-    def get_participants(self) -> set[ContestParticipant]:
+    def get_participants(self) -> set[Student]:
         return {submission.author for submission in self.submissions}
 
 
 class Submission(BaseModel):
     id: int
-    author: ContestParticipant
+    author: Student
     verdict: Verdict
     passed_test_count: int
     points: float | None
     programming_language: str
 
     def map_author_handle_to_email(self, handle_to_email_mapper: Callable[[str], EmailStr | None]) -> None:
-        if isinstance(self.author, Student):
-            return
-
-        email = handle_to_email_mapper(self.author.handle)
-        if email is not None:
-            self.author = Student(
-                handle=self.author.handle,
-                email=email
-            )
+        if self.author.email is None:
+            self.author.email = handle_to_email_mapper(self.author.handle)
