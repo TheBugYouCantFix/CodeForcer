3b4a874c517df22a6ff86fb70c1b1e7f25bbb4a9
mashfeii_main <evil-corporaty@yandex.ru>
2024-07-01 00:10:21 +0300

    Removed Moodle page, added text for user help

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Card from "../ui/Card.jsx";
 import { HiHome } from "react-icons/hi2";
 import { SiMoodle, SiCodeforces } from "react-icons/si";
 import { FaDatabase } from "react-icons/fa6";
+import { Description } from "../ui/Description.jsx";
 
 function Home() {
   return (
@@ -12,13 +13,27 @@ function Home() {
         <HiHome />
         Home page
       </Heading>
+      <Description>
+        <strong>
+          Welcome to our website! Here you can easily manage CodeForces handlers
+          and get the results of the contest you are interested in, easily find
+          inconsistencies in handlers and distribute points for completing
+          specific tasks. Enjoy easy navigation and efficient controls at your
+          fingertips!
+        </strong>
+        {/*<span>
+          For students, after logging into the university system, you can enter
+          your CodeForces username so that the TA responsible for checking can
+          put your grade.
+        </span>*/}
+      </Description>
       <Row type="filled">
         <Card
           title="Download Submissions"
           icon={SiCodeforces}
           to="/submissions"
         />
-        <Card title="Submit Grades" icon={SiMoodle} to="/submit" />
+        {/*<Card title="Submit Grades" icon={SiMoodle} to="/submit" />*/}
         <Card title="Edit Handles" icon={FaDatabase} to="/handles" />
       </Row>
     </>
