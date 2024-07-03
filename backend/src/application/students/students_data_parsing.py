c76c3e65835347ad59709c31d1daf50f0b9175ff
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-03 05:06:17 +0300

    refactor: created match case expression for file extensions

--- backend/src/application/students/students_data_parsing.py
+++ backend/src/application/students/students_data_parsing.py
@@ -1,9 +1,19 @@
 import csv
+import os
 
 from contracts.student_data import StudentData
 
 
 def parse_students_data(path: str) -> list[StudentData]:
+    extension = os.path.splitext(path)[-1]
+    match extension:
+        case ".csv":
+            return __parse_students_data_from_csv(path)
+        case _:
+            pass  # TODO raise unsupported file extension exception
+
+
+def __parse_students_data_from_csv(path: str) -> list[StudentData]:
     with open(path, mode='r') as file:
         csv_reader = csv.DictReader(file)
 
