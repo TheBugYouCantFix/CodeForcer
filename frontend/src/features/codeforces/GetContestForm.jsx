7079cb9e40dd89358a7b33eb7e8f93d45e468cd5
mashfeii-laptop <evil-corporaty@yandex.ru>
2024-07-06 05:00:54 +0300

    refactor: changed structure for component, made several separate instead

--- frontend/src/features/codeforces/GetContestForm.jsx
+++ frontend/src/features/codeforces/GetContestForm.jsx
@@ -1,37 +1,18 @@
 import { useForm } from "react-hook-form";
-import { useState } from "react";
-import SumbissionInfo from "../../ui/SumbissionInfo.jsx";
 import { useLocalStorageState } from "../../hooks/useLocalStorage.js";
 import Form from "../../ui/Form.jsx";
 import Input from "../../ui/Input.jsx";
 import FormElement from "../../ui/FormElement.jsx";
 import Button from "../../ui/Button.jsx";
+import { useNavigate, useNavigation } from "react-router-dom";
 import SpinnerMini from "../../ui/SpinnnerMini.jsx";
-import toast from "react-hot-toast";
 
 function GetContestForm() {
-  const [contestInfo, setContestInfo] = useState({});
   const [id, setId] = useLocalStorageState("", "id");
   const [api, setApi] = useLocalStorageState("", "api");
   const [secret, setSecret] = useLocalStorageState("", "secret");
-
-  const [isGetting, setIsGetting] = useState(false);
-  async function getContest(url) {
-    setIsGetting(true);
-    const response = await fetch(url, {
-      // mode: "no-cors",
-    });
-    console.log(response);
-    if (response.status >= 500) {
-      throw new Error("Something went wrong with CodeForces");
-    }
-    if (!response.ok) {
-      throw new Error("Something went wrong!");
-    }
-
-    const data = response.json();
-    return data;
-  }
+  const navigate = useNavigate();
+  const navigation = useNavigation();
 
   const {
     register,
@@ -57,58 +38,40 @@ function GetContestForm() {
     setApi(APIKey);
     setSecret(secretKey);
 
-    // Sendind GET request
-    // `https://13bd4039-c465-4f54-b961-30824dc7cc9b.mock.pstmn.io/contests/52`
-    const url = `http://10.90.137.106:8000/contests/${contestID}?key=${APIKey}&secret=${secretKey}`;
-    getContest(url)
-      .then((data) => {
-        setContestInfo(data);
-        setIsGetting(false);
-      })
-      .catch((err) => {
-        toast.error(err.message);
-        setIsGetting(false);
-      });
+    navigate(`${contestID}`);
   };
 
   return (
-    <>
-      {Object.keys(contestInfo).length === 0 && (
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <FormElement label="Contest URL" error={errors?.contestID?.message}>
-            <Input
-              placeholder="ID or full link"
-              disabled={isGetting}
-              {...register("contestID", { required: "This field is required" })}
-            />
-          </FormElement>
-          <FormElement
-            label="Codeforces API"
-            error={errors?.APIKey?.message || errors?.secretKey?.message}
-          >
-            <Input
-              placeholder="API Key"
-              disabled={isGetting}
-              {...register("APIKey", { required: "These fields are required" })}
-            />
-            <Input
-              placeholder={"Secret Key"}
-              disabled={isGetting}
-              {...register("secretKey", {
-                required: "These fields are required",
-              })}
-            />
-          </FormElement>
-          <Button disabled={isGetting} type="submit">
-            {isGetting ? <SpinnerMini /> : "Submit"}
-          </Button>
-        </Form>
-      )}
-
-      {Object.keys(contestInfo).length !== 0 && (
-        <SumbissionInfo info={contestInfo} reset={setContestInfo} />
-      )}
-    </>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormElement label="Contest URL" error={errors?.contestID?.message}>
+        <Input
+          placeholder="ID or full link"
+          disabled={navigation.state !== "idle"}
+          {...register("contestID", { required: "This field is required" })}
+        />
+      </FormElement>
+      <FormElement
+        label="Codeforces API"
+        disabled={navigation.state !== "idle"}
+        error={errors?.APIKey?.message || errors?.secretKey?.message}
+      >
+        <Input
+          placeholder="API Key"
+          disabled={navigation.state !== "idle"}
+          {...register("APIKey", { required: "These fields are required" })}
+        />
+        <Input
+          placeholder={"Secret Key"}
+          disabled={navigation.state !== "idle"}
+          {...register("secretKey", {
+            required: "These fields are required",
+          })}
+        />
+      </FormElement>
+      <Button disabled={navigation.state !== "idle"} type="submit">
+        {navigation.state !== "idle" ? <SpinnerMini /> : "Submit"}
+      </Button>
+    </Form>
   );
 }
 
