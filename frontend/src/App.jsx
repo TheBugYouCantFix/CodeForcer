35c0f7e633b42036bab0bd32b290b1a715f85624
mashfeii-laptop <evil-corporaty@yandex.ru>
2024-07-06 18:47:38 +0300

    lol: returned toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Submissions from "./pages/Submissions.jsx";
 import { DarkModeProvider } from "./context/DarkModeContext.jsx";
 import Settings, { loader as settingsLoader } from "./pages/Setting.jsx";
 import ErrorFallback from "./ui/ErrorFallback.jsx";
+import { Toaster } from "react-hot-toast";
 
 const router = createBrowserRouter([
   {
@@ -46,6 +47,27 @@ function App() {
     <DarkModeProvider>
       <GlobalStyles />
       <RouterProvider router={router} />
+      <Toaster
+        position="top-center"
+        reverseOrder={true}
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "1.6rem",
+            maxWidth: "50rem",
+            padding: "1.6rem 2.4rem",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </DarkModeProvider>
   );
 }
