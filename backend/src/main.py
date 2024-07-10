f9ed6482d923c1b4367c88a880f142a0c3e2128b
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-11 00:51:30 +0300

    chore: added another origin for CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -16,7 +16,8 @@ app.include_router(moodle_grades_router)
 origins = [
     "http://0.0.0.0:3000",
     "http://127.0.0.1:3000",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://10.90.138.143:3000"
 ]
 
 app.add_middleware(
