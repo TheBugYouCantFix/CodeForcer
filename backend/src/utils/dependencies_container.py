162eaa833142c14c082aea58d02c9001ea8f0308
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-11 21:35:00 +0300

    fix: removed deprecated Hashable

--- backend/src/utils/dependencies_container.py
+++ backend/src/utils/dependencies_container.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Callable, Hashable
+from typing import TypeVar, Callable
 
 T = TypeVar("T")
 
@@ -6,10 +6,10 @@ T = TypeVar("T")
 class DependenciesContainer:
     dependencies: dict[type, Callable[[], T] | T] = {}
 
-    def __setitem__(self, key: type[T] | Hashable, dependency: Callable[[], T] | T):
+    def __setitem__(self, key: type[T], dependency: Callable[[], T] | T):
         self.dependencies[key] = dependency
 
-    def __getitem__(self, key: type[T] | Hashable) -> T:
+    def __getitem__(self, key: type[T]) -> T:
         if key in self.dependencies:
             dependency = self.dependencies[key]
             return dependency() if callable(dependency) else dependency
