ae78bd3c59149322d02431e7182dad974571334d
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-14 00:04:56 +0300

    fix: deleted unneeded CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
@@ -12,21 +11,6 @@ app = FastAPI()
 app.include_router(students_router)
 app.include_router(contests_router)
 app.include_router(moodle_grades_router)
-
-origins = [
-    "http://0.0.0.0:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-    "http://10.90.138.143:3000"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.exception_handler(StarletteHTTPException)
