c0a3660a2b998cc7c0449fd38f0bd2f7e645696f
alexg <alex.gorbanev05@gmail.com>
2024-07-16 06:13:28 +0500

    refactor: applied the invalid_email fixture

--- backend/tests/students_test/test_update_or_create_student.py
+++ backend/tests/students_test/test_update_or_create_student.py
@@ -42,15 +42,15 @@ def test_updates_existing_student_if_data_is_valid(email, handle, contests_provi
     assert students_repo_mock.get_student_by_email(email).model_dump() == request_data
 
 
-def test_does_not_update_student_if_email_is_invalid(email, handle, contests_provider_mock):
+def test_does_not_update_student_if_email_is_invalid(invalid_email, handle, contests_provider_mock):
     contests_provider_mock.valid_handles = [handle]
 
     request_data = {
-        "email": email + '!',
+        "email": invalid_email,
         "handle": handle
     }
 
-    response = client.put(f"/students/{email}", json=request_data)
+    response = client.put(f"/students/{invalid_email}", json=request_data)
 
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
