e69f44d9b4d01d31138e9e01e9127de95957745a
alexg <alex.gorbanev05@gmail.com>
2024-07-16 05:55:44 +0500

    feat: finished the tests for the put "update or create student" endpoint

---
+++ backend/tests/students_test/test_update_or_create_student.py
@@ -0,0 +1,66 @@
+import pytest
+from fastapi import status
+
+from tests.create_test_client import client
+
+
+@pytest.fixture(autouse=True)
+def setup_and_teardown(contests_provider_mock, students_repo_mock):
+    yield
+    contests_provider_mock.valid_handles = []
+    students_repo_mock.db.clear()
+
+
+def test_creates_student_if_not_exists_and_data_is_valid(email, handle, contests_provider_mock, students_repo_mock):
+    contests_provider_mock.valid_handles = [handle]
+
+    request_data = {
+        "email": email,
+        "handle": handle
+    }
+
+    response = client.put(f"/students/{email}", json=request_data)
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert students_repo_mock.get_student_by_email(email).model_dump() == request_data
+
+
+def test_does_not_update_or_create_if_handle_is_invalid(email, handle, contests_provider_mock):
+    request_data = {
+        "email": email,
+        "handle": handle
+    }
+
+    response = client.put(f"/students/{email}", json=request_data)
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_updates_existing_student_if_data_is_valid(email, handle, contests_provider_mock, students_repo_mock):
+    contests_provider_mock.valid_handles = [handle]
+
+    request_data = {
+        "email": email,
+        "handle": handle
+    }
+
+    response = client.put(f"/students/{email}", json=request_data)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert students_repo_mock.get_student_by_email(email).model_dump() == request_data
+
+
+def test_does_not_update_student_if_email_is_invalid(email, handle, contests_provider_mock):
+    contests_provider_mock.valid_handles = [handle]
+
+    request_data = {
+        "email": email + '!',
+        "handle": handle
+    }
+
+    response = client.put(f"/students/{email}", json=request_data)
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+
