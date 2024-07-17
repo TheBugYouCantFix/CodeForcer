738c233e3adcfaa4ec512ceb9c913b6d954bdcca
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-17 22:06:03 +0300

    feat: contests attribute for ContestsProviderMock

--- backend/tests/mocks/contests_provider_mock.py
+++ backend/tests/mocks/contests_provider_mock.py
@@ -4,10 +4,11 @@ from src.features.contests.models import Contest
 
 class ContestsProviderMock(IContestsProvider):
     def __init__(self):
+        self.contests: dict[int, Contest] = {}
         self.valid_handles = []
 
     def get_contest(self, contest_id: int, api_key: str, api_secret: str) -> Contest:
-        pass
+        return self.contests[contest_id]
 
     def get_contest_results(self, contest_id: int, api_key: str, api_secret: str):
         pass
