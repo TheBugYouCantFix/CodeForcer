68603f769dfd5b973ed98f21f4ee9f25d9c8a777
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-17 22:09:26 +0300

    test: successful create of update_or_create_student

--- backend/tests/students_test/test_update_or_create_student.py
+++ backend/tests/students_test/test_update_or_create_student.py
@@ -29,7 +29,7 @@ def test_returns_400_bad_request_if_handle_is_invalid(email, invalid_handle):
     assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
-def test_updates_existing_student_if_data_is_valid_and_student_exists(email, handle, students_repo_mock):
+def test_updates_student_if_data_is_valid_and_student_exists(email, handle, students_repo_mock):
     request_data = {
         "email": email,
         "handle": handle
@@ -40,6 +40,18 @@ def test_updates_existing_student_if_data_is_valid_and_student_exists(email, han
     response = client.put(f"/students/{email}", json=request_data)
 
     assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert students_repo_mock.get_student_by_email(email).model_dump() == request_data
+
+
+def test_creates_new_student_if_data_is_valid_and_student_does_not_exist(email, handle, students_repo_mock):
+    request_data = {
+        "email": email,
+        "handle": handle
+    }
+
+    response = client.put(f"/students/{email}", json=request_data)
+
+    assert response.status_code == status.HTTP_201_CREATED
     assert students_repo_mock.get_student_by_email(email).model_dump() == request_data
 
 
