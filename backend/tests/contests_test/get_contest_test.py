ccdc306a60245479bd85d3acfe2a137ec0679a27
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-18 00:46:27 +0300

    test: gets_contest_with_mapped_emails_foreach_student

--- backend/tests/contests_test/get_contest_test.py
+++ backend/tests/contests_test/get_contest_test.py
@@ -1,7 +1,9 @@
 from fastapi import status
 
 from src.features.contests.models import Contest
+from src.features.students.model import Student
 from tests.create_test_client import client
+from tests.mocks.data_generation import fake
 
 
 def test_gets_contest_when_contest_exists(contest):
@@ -46,3 +48,24 @@ def test_gets_contest_with_single_submission_foreach_student(contest):
             assert response_submission.author.handle not in submission_authors
 
             submission_authors.add(response_submission.author.handle)
+
+
+def test_gets_contest_with_mapped_emails_foreach_student(contest, students_repo_mock):
+    students_by_email = {
+        (fake_email := fake.email()): Student(email=fake_email, handle=student.handle)
+        for student
+        in contest.get_participants
+        if student.email is None
+    }
+
+    students_repo_mock.db |= students_by_email
+
+    response = client.get(f"/contests/{contest.id}", params={"key": "", "secret": ""})
+
+    assert response.status_code == status.HTTP_200_OK
+
+    response_contest = Contest.model_validate_json(response.text)
+    for student in response_contest.get_participants:
+        assert student.email is not None
+        if student.email in students_by_email:
+            assert student == students_by_email[student.email]
