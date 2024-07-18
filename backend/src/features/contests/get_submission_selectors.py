6f8c1405ea28382cd2adf84617dfcb50d1ae66aa
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-18 23:01:06 +0300

    refactor: removed contests/ from endpoint name

--- backend/src/features/contests/get_submission_selectors.py
+++ backend/src/features/contests/get_submission_selectors.py
@@ -6,6 +6,6 @@ from .submission_selectors import submission_selectors
 router = APIRouter()
 
 
-@router.get("/contests/submission-selectors", status_code=status.HTTP_200_OK)
-async def get_contests_submission_selectors() -> list[str]:
+@router.get("/submission-selectors", status_code=status.HTTP_200_OK)
+async def get_submission_selectors() -> list[str]:
     return list(submission_selectors.keys())
--- backend/src/features/contests/route.py
+++ backend/src/features/contests/route.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter
 
 from .get_contest import router as get_contest_router
 from .get_contest_results import router as get_contest_results_router
-from .get_contests_submission_selectors import router as get_contests_selectors_router
+from .get_submission_selectors import router as get_submissions_selectors_router
 
 router = APIRouter()
 
 router.include_router(get_contest_router)
 router.include_router(get_contest_results_router)
-router.include_router(get_contests_selectors_router)
+router.include_router(get_submissions_selectors_router)
