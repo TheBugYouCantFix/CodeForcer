90892df62e455fe07240e002743398096def35fd
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-18 23:09:01 +0300

    fix: submission selector name validator

--- backend/src/features/contests/submission_selectors.py
+++ backend/src/features/contests/submission_selectors.py
@@ -4,13 +4,19 @@ from pydantic.functional_validators import AfterValidator
 
 from src.features.contests.models import Submission
 
+
+def submission_name_validator(name: str) -> str:
+    assert name in submission_selectors, f"Invalid submission selector name"
+    return name
+
+
 SubmissionSelector = Callable[[list[Submission]], Submission]
-SubmissionSelectorName = Annotated[str, AfterValidator(lambda name: name in submission_selectors)]
+SubmissionSelectorName = Annotated[str, AfterValidator(submission_name_validator)]
 
 submission_selectors: dict[str, SubmissionSelector] = {}
 
 
-def submissions_selector(name: str):
+def submission_selector(name: str):
     def decorator(selector: SubmissionSelector):
         submission_selectors[name] = selector
         return selector
@@ -18,22 +24,22 @@ def submissions_selector(name: str):
     return decorator
 
 
-@submissions_selector("most passed test count")
+@submission_selector("most passed test count")
 def most_passed_test_count_selector(submissions: list[Submission]) -> Submission:
     return max(submissions, key=lambda s: (s.passed_test_count, s.submission_time_utc))
 
 
-@submissions_selector("latest")
+@submission_selector("latest")
 def latest_submission_selector(submissions: list[Submission]) -> Submission:
     return max(submissions, key=lambda s: s.submission_time_utc)
 
 
-@submissions_selector("latest successful")  # returns latest unsuccessful submission if no successful submissions
+@submission_selector("latest successful")  # returns latest unsuccessful submission if no successful submissions
 def latest_submission_selector(submissions: list[Submission]) -> Submission:
     return max(submissions, key=lambda s: (s.is_successful, s.submission_time_utc))
 
 
-@submissions_selector("most points")
+@submission_selector("most points")
 def most_points_selector(submissions: list[Submission]) -> Submission:
     return max(submissions, key=lambda s: (s.points, s.submission_time_utc))
 
