f1ea5270a1b9d8555cd73b2981ae57db16e848ab
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-18 22:50:21 +0300

    feat: submissions selector name as an argument for GET contests/{id}

--- backend/src/features/contests/get_contest.py
+++ backend/src/features/contests/get_contest.py
@@ -4,18 +4,23 @@ from starlette import status
 
 from src.container import container
 from src.features.students.interfaces import IStudentsRepository
-from .models import Contest, Submission
+from .submission_selectors import submission_selectors, SubmissionSelectorName
 from .interfaces import IContestsProvider
+from .models import Contest
 
 router = APIRouter()
 
 
 @router.get("/contests/{contest_id}", status_code=status.HTTP_200_OK)
-async def get_contest(contest_id: int, key: str, secret: str) -> Contest:
+async def get_contest(
+        contest_id: int,
+        key: str, secret: str,
+        submission_selector_name: SubmissionSelectorName = 'latest'
+) -> Contest:
     return GetContestQuery(
         container[IContestsProvider],
         container[IStudentsRepository]
-    ).handle(contest_id, key, secret)
+    ).handle(contest_id, key, secret, submission_selector_name)
 
 
 class GetContestQuery:
@@ -26,10 +31,10 @@ class GetContestQuery:
         self.contests_provider = contests_provider
         self.students_repository = students_repository
 
-    def handle(self, contest_id: int, api_key: str, api_secret: str) -> Contest:
+    def handle(self, contest_id: int, api_key: str, api_secret: str, submission_selector_name: str) -> Contest:
         contest = self.contests_provider.get_contest(contest_id, api_key, api_secret)
 
-        contest.select_single_submission_for_each_participant(selector=most_passed_test_count_selector)
+        contest.select_single_submission_for_each_participant(selector=submission_selectors[submission_selector_name])
         contest.map_handles_to_emails(handle_to_email_mapper=self._get_email_by_handle)
 
         return contest
@@ -37,11 +42,3 @@ class GetContestQuery:
     def _get_email_by_handle(self, handle) -> EmailStr | None:
         student = self.students_repository.get_student_by_handle(handle)
         return student.email if student is not None else None
-
-
-def most_passed_test_count_selector(submissions: list[Submission]) -> Submission:
-    return max(submissions, key=lambda s: s.passed_test_count)
-
-
-def latest_submission_selector(submissions: list[Submission]) -> Submission:
-    return submissions[-1]
