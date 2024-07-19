fe2d592276cb9723d3061187ed2b2ead2f0028a1
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-19 23:08:43 +0300

    refactor: remove ignored exceptions handler

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from fastapi.exceptions import HTTPException
 from uvicorn import run
 
 from src.features.students.route import router as students_router
@@ -12,14 +10,6 @@ app = FastAPI(title='CodeForcer')
 app.include_router(students_router, prefix='/api/students')
 app.include_router(contests_router, prefix='/api/contests')
 app.include_router(moodle_grades_router, prefix='/api/moodle-grades')
-
-
-@app.exception_handler(HTTPException)
-async def http_exception_handler(_, exc: HTTPException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"message": str(exc)},
-    )
 
 
 if __name__ == '__main__':
