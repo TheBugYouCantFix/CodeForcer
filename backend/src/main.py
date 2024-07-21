913db721417db7aca77fa40a78747b82f676c4d9
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-21 22:49:23 +0300

    fix: route

--- backend/src/main.py
+++ backend/src/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title='CodeForcer')
 app.include_router(students_router, prefix='/api/students')
 app.include_router(contests_router, prefix='/api/contests')
 app.include_router(moodle_grades_router, prefix='/api/moodle-grades')
-app.include_router(submissions_archive_router, prefix='/api/')
+app.include_router(submissions_archive_router, prefix='/api')
 
 
 if __name__ == '__main__':
