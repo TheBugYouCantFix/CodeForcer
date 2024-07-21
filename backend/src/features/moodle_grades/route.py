7d8b088baaeeb181de9827a107aead949a3a8a17
Daniel Gevorgyan <gevorgyan@mts.ru>
2024-07-21 23:51:43 +0300

    chore: new router

--- backend/src/features/moodle_grades/route.py
+++ backend/src/features/moodle_grades/route.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from .create_grades_file import router as create_grades_router
 from .get_submission_selectors import router as submission_selectors_router
 from .parse_legal_excuses_table import router as parse_legal_excuses_table_router
+from .submissions_archive import router as submissions_archive_router
 
 router = APIRouter()
 
 router.include_router(create_grades_router)
 router.include_router(submission_selectors_router)
 router.include_router(parse_legal_excuses_table_router)
+router.include_router(submissions_archive_router)
